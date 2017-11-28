236a12d3bb4a18d36fe27b1526198394ca4be9b9
PetrusStarken <pedro@blockoventures.com>
2017-11-28 17:43:43 -0200

    create crowdsale

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,18 @@
 var SafeMath = artifacts.require("./SafeMath.sol");
 var CookieToken = artifacts.require("./CookieToken.sol");
+var Crowdsale = artifacts.require("./Crowdsale.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(SafeMath);
   deployer.link(SafeMath, CookieToken);
-  deployer.deploy(CookieToken);
+  deployer.deploy(CookieToken).then( () => {
+    return deployer.deploy(
+      Crowdsale,
+      CookieToken.address,
+      web3.eth.blockNumber,
+      web3.eth.blockNumber+1000,
+      web3.toWei(1, 'ether'),
+      1
+    ).then(()=>{});
+  });
 };
